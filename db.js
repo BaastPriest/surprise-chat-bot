2866fceb722da5852ddd45cea61506e21bd7cc1b
BaastPriest <Aul0na@rambler.ru>
2025-09-08 17:12:41 +0400

    fixed db connection

--- db.js
+++ db.js
@@ -9,6 +9,7 @@ if (usePostgres) {
     pool = new Pool({
         connectionString: process.env.DATABASE_URL,
         ssl: process.env.PGSSL === 'true' ? { rejectUnauthorized: false } : false,
+        connectionTimeoutMillis: 5000,
     });
 }
 
